4f7ff72902bfe3d6bad4edee89ea4d21b0b5e5b9
Minhaj9800 <ridwanminhaj@gmail.com>
2022-02-28 22:17:36 -0400

    adding permission in the android/app.src/debug/AndroidMainfest.xml to use bluetooth and access location

--- lib/pages/bluetooth_function/bluettoth_function.dart
+++ lib/pages/bluetooth_function/bluettoth_function.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_blue/flutter_blue.dart';
 
-//FlutterBlue flutterBlueInstance  = FlutterBlue.instance;
+// Flutter Blue Instance to access the flutter_blue plug in from library
+FlutterBlue flutterBlueInstance  = FlutterBlue.instance;
